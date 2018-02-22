dfe988c289c0a2fb57b203842d6caf4bcb7a41f8
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-02-22 23:23:44 +0100

    Comp-graphics lab3 regional_tree implement search method

--- comp-graphics/lab3/regional_tree.rb
+++ comp-graphics/lab3/regional_tree.rb
@@ -14,4 +14,25 @@ class RegionalTree < SectionTree
     insert_points(new_points, node.left)
     insert_points(new_points, node.right)
   end
+
+  def search(quad)
+    min_x = quad.map(&:x).min
+    max_x = quad.map(&:x).max
+    pr = PointRange.new(min_x, max_x)
+    b_search(tree, pr)
+  end
+
+  def b_search(node, pr)
+    return [] if node.nil?
+    result = []
+    if node.obj.close <= pr.close && node.obj.beg >= pr.beg
+      result = node.obj.points
+    else
+      result += b_search(node.left, pr) if node.obj.beg < pr.close
+      result += b_search(node.right, pr) if node.obj.close > pr.beg
+    end
+    result
+  end
+
+  private :b_search
 end
